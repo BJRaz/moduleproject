b193b8fd0c3418cc8f38784f93685a88bea94cab
Brian Juul Rasmussen <brian.juul.rasmussen@gmail.com>
2021-02-13 13:01:25 +0100

    Added dependency to stringmodule Added sourcemaps in tsconfig.json Updated .gitignore to exclude map-files

--- main.ts
+++ main.ts
@@ -2,26 +2,59 @@ import { StringUtils } from "./lib/stringutils"     // local library/module
 import { Horse, MyList } from "./mymodule"          //       --
 import * as sm from "stringmodule";                 // external library/module
 
+
+function* f() {
+    for(let i=0;i<10;i++)
+        yield i;
+};
+
+class Hest<T>
+{
+    public where(cb: (arg: T) => {}) {
+        
+        return function* f() {
+            yield 1;
+        };
+    }
+}
+
 class Main
 {
+    async getData() {
+        return await setTimeout(() => 10, 2000);
+    }
+
     public static Start() : number {
 
-        console.log("Start");
-
-        console.log(StringUtils.Length("A simple string..."));
-
-        console.log(sm.returnString());
-
-        console.log(sm.reverseString(sm.returnString()));
-
-        var h = new Horse();
-        h.findHorse();
-        
-        var list = new MyList([1,2,3,4,5]);
-        
-        for (const obj of list) {
-            console.log(obj);
+        for (const iterator of f()) {
+            console.log(iterator);
         }
+        // var m = new Main();
+        // var hest = m.getData().then(v => {
+        //     console.log(v);
+        // });
+
+        
+
+        // console.log(hest);
+
+        // console.log("Start");
+
+        // console.log(StringUtils.Length("A simple string..."));
+
+        // console.log(sm.returnString());
+
+        // console.log(sm.reverseString(sm.returnString()));
+
+        // var h = new Horse();
+        // h.findHorse();
+
+        
+        // var list = new MyList<string>(["1","2","3","4","5"]);
+        
+        // for (const obj of list) {
+        //     console.log(obj);
+        // }
         
         return 0;
     }
